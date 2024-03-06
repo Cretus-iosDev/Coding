e8820d17688082d17adebe92789ef1ba2fea3a76
Rutik Maraskolhe <84556881+Cretus-iosDev@users.noreply.github.com>
2024-03-06 18:32:05 +0530

    Add files via upload

---
+++ Learn the Basics/array.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+1D- Array
+
+int main() {
+    int arr[5];
+    cin >> arr[0] >> arr[1] >> arr[2] >> arr[3] >> arr[4];
+
+    cout << arr[3] << endl;
+
+    return 0 ;
+}
+*/
+
+/*
+2D-Array
+int main() {
+    int arr[3][5];
+
+    arr[1][3] = 78;
+    cout << arr[1][3] << endl;
+    return 0;
+}
+
+*/
+
