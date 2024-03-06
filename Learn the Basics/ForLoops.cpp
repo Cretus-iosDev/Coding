49233725c592fb862dbde200d989dfc507325b00
Rutik Maraskolhe <84556881+Cretus-iosDev@users.noreply.github.com>
2024-03-06 18:56:56 +0530

    Add files via upload

---
+++ Learn the Basics/ForLoops.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int i;
+    for(i=1; i <= 5; i = i+1){
+        cout << "rutik" << endl;
+    }
+
+    for(i=5; i > 0; i = i - 1){
+        cout << "rutik" << i << endl;
+    }
+    cout << i << endl;
+    return 0;
+}
